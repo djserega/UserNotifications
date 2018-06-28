9a72674a50af624b78ac730d2a1d1a31dbadf32d
dj_serega <galyuk.sergey@gmail.com>
2018-06-28 19:06:01 +0300

    add proj caller com

---
+++ src/UserNotificationsCaller/Program.cs
@@ -0,0 +1,19 @@
+﻿using UserNotifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserNotificationsCaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notifications com = new Notifications();
+            com.SetTitle("c#7");
+            com.Show("time now: " + DateTime.Now.ToString("s"));
+        }
+    }
+}
